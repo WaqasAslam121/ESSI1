038b9acf1b8f529c0959cd8b9a847d40dfb7536c
WaqasAslam121 <97800099+WaqasAslam121@users.noreply.github.com>
2023-03-02 22:59:54 +0500

    .

--- App/blog/database.py
+++ App/blog/database.py
@@ -2,17 +2,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Netsolpk1@localhost:5432/Blog"
-
+'''
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
 '''
+'''
+# p1008057155153-qa0qp8@gcp-sa-cloud-sql.iam.gserviceaccount.com
 engine = create_engine(
     "postgresql://postgres:Netsolpk1@localhost:5432/Blog")
 '''
+
+engine = create_engine(
+    "postgresql://postgresessi:978K0RHW5L2UJVpWbQmzsWezC1QMi2Dg@dpg-cg0dk45269vdqr9mpt10-a.oregon-postgres.render.com/blog_1bqo")
+
+#postgres://postgresessi:978K0RHW5L2UJVpWbQmzsWezC1QMi2Dg@dpg-cg0dk45269vdqr9mpt10-a.oregon-postgres.render.com/blog_1bqo
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
